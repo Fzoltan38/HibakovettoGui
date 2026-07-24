35c87f3ffd6557a149f1d8c5d7ea3b998d80c137
FZoltan <farkaszoltan28@gmail.com>
2026-07-24 16:59:07 +0200

    HibaKoveto DashBoardPage first row is ready.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HibaKovetoWpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
