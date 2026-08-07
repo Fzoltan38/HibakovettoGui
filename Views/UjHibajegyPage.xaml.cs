46d6bb5a3d99e45cd64117190aa0dc79ff3a22d6
FZoltan <farkaszoltan28@gmail.com>
2026-08-07 18:32:44 +0200

    UjHibajegyPage is ready to use.

--- Views/UjHibajegyPage.xaml.cs
+++ Views/UjHibajegyPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HibaKovetoWpf.Models;
+using HibaKovetoWpf.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -21,6 +23,43 @@ namespace HibaKovetoWpf.Views
         public UjHibajegyPage()
         {
             InitializeComponent();
+        }
+
+        private void MentesGomb_Click(object sender, RoutedEventArgs e)
+        {
+            string cim = CimBox.Text.Trim();
+            string leiras = LeirasBox.Text.Trim();
+
+            if (cim.Length == 0 || leiras.Length == 0)
+            {
+                HibaText.Text = "A cím és a leírás nem lehet üres!";
+                HibaText.Visibility = Visibility.Visible;
+                return;
+            }
+
+            var komponensItem = KomponensCombo.SelectedItem as ComboBoxItem;
+            ComboBoxItem? sulyossagItem = SulyossagCombo.SelectedItem as ComboBoxItem;
+
+            Hibajegy ujHiba = new Hibajegy
+            {
+                Cim = cim,
+                Leiras = leiras,
+                Komponens = komponensItem != null ? komponensItem.Content.ToString() : "",
+                Sulyossag = sulyossagItem != null ? sulyossagItem.Content.ToString() : "",
+                SurgosBeavatkozas = SurgosCheckBox.IsChecked == true,
+                Megoldva = false,
+                Bejelentve = DateTime.Now
+            };
+
+            HibaTar.HibajegyHozzaadas(ujHiba);
+
+            NavigationService.Navigate(new DashBoardPage());
+
+        }
+
+        private void MegseGomb_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new DashBoardPage());
         }
     }
 }
